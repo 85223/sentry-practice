c0f03219ee9337d332a5d8e50046ca3fbf4432f1
yuhsiang <H24364032@gmail.com>
2023-05-02 23:29:26 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_CDN,
+  tracesSampleRate: 1.0,
+  integrations: [new sentry.BrowserTracing()],
+});
+
+const FallbackComponent = () => {
+  return (
+    <>
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          background: "red",
+          position: "absolute",
+          zIndex: "100",
+        }}
+      >
+        An error has occurred
+      </div>
+    </>
+  );
+};
+
+const Test = () => {
+  const handleClick = () => {
+    try {
+      throw new Error("Test Error！");
+    } catch (error: any) {
+      // 捕獲錯誤並發送到 Sentry
+      sentry.configureScope((scope) => {
+        sentry.setContext("Error-Test", error);
+        sentry.setTag("Error-Test-Tags", "Error-Test");
+        sentry.captureException(error);
+      });
+      console.log(error);
+
+      sentry.captureException(error);
+      sentry.showReportDialog({ eventId: sentry.lastEventId() });
+    }
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Switch</button>
+    </>
+  );
+};
+
+const myFallback = <FallbackComponent />;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <sentry.ErrorBoundary fallback={myFallback}>
+      <Test />
+
+      <App />
+    </sentry.ErrorBoundary>
   </React.StrictMode>
 );
 
